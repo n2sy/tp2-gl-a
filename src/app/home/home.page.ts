81b2aa1792418d7f25fced461feccab77b29488d
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-09-23 10:14:25 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListCoursesService } from '../list-courses.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  tabCourses = [];
 
-  constructor() {}
+  constructor(private courseSer: ListCoursesService) {}
 
+  ngOnInit() {
+    this.tabCourses = this.courseSer.getAllCourses();
+  }
 }
